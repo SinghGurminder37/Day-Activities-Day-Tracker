e701f9a297b49289bc5783446fc85e88145b1e41
Gurminder Singh <gurminder037@gmail.com>
2020-06-07 14:59:37 -0700

    Add activity to UI

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Storage controller
+// Storage controller #########################
 const StorageCtrl = function(){
 return{
 
@@ -8,17 +8,17 @@ return{
 // Activity Controlle  ########################
 const ActivityCtrl = function(){
     //Activity Constructor
-    const Activity = function(id, activity, time){
+    const Activity = function(id, name, time){
         this.id = id;
-        this.activity = activity;
+        this.name = name;
         this.time = time;
     }
 
     const data = {
         activities: [
-            {id: 0, name: "breakfast", time: "7 am"},
-            {id: 1, name: "Get Ready", time: "7:30 am"},
-            {id: 2, name: "Go to Work", time: "8:00 am"}
+            // {id: 0, name: "breakfast", time: "7 am"},
+            // {id: 1, name: "Get Ready", time: "7:30 am"},
+            // {id: 2, name: "Go to Work", time: "8:00 am"}
         ],
         currentActivity: null,
         totalActivities: 0
@@ -30,17 +30,19 @@ const ActivityCtrl = function(){
         },
         addActivity: function(name, time){
            let ID;
-           if(data.activities.length >0){
-            ID = data.activities[data.activities.length -1].id +1;
+        //    Create ID
+           if(data.activities.length > 0){
+            ID = data.activities[data.activities.length - 1].id +1;
            }else{
             ID=0;
            }
         
             // Create new activity with ID
-            const newActivity = new Activity(ID, name, time);
+            newActivity = new Activity(ID, name, time);
 
             // Push new created activity into array
             data.activities.push(newActivity);
+            return newActivity;
 
         },
         getActivities: function(){
@@ -50,7 +52,7 @@ const ActivityCtrl = function(){
     }    
 }();
 
-// UI Controller ###########################
+// UI Controller ##############################
 const UICtrl = function(){
     const UISelectors = {
         activityList: '#activity-list',
@@ -60,6 +62,30 @@ const UICtrl = function(){
     }
     
     return{
+        hideUIList: function(){
+            document.querySelector(UISelectors.activityList).style.display = 'none';
+        },
+        clearInput: function(){
+            document.querySelector(UISelectors.activityName).value = '',
+            document.querySelector(UISelectors.activityTime).value = ''
+        },
+        addUIActivity: function(newActivity){
+            // Show UI List
+            document.querySelector(UISelectors.activityList).style.display = 'block';
+            // Create li element
+            const li = document.createElement('li');
+            // Add class name
+            li.className = 'collection-item';
+            // Add id
+            li.id = `item-${newActivity.id}`;
+            // Add HTML
+            li.innerHTML = `<strong>${newActivity.name}: </strong> <em>${newActivity.time}</em>
+            <a href="#" class="secondary-content">
+                <i class="edit-activity fa fa-pencil"></i>
+            </a>`;
+            // Insert item
+            document.querySelector(UISelectors.activityList).insertAdjacentElement('beforeend', li);
+        },
         populateListAct: function(activities){
             let html = '';
 
@@ -78,7 +104,7 @@ const UICtrl = function(){
         getActivityInput: function(){
             return{
                 name:document.querySelector(UISelectors.activityName).value,
-                time:document.querySelector(UISelectors.activityName).value
+                time:document.querySelector(UISelectors.activityTime).value
             }
         },
         getUISelectors: function(){
@@ -87,7 +113,7 @@ const UICtrl = function(){
     }
 }();
 
-// App Controller #########################
+// App Controller #############################
 const App = function(ActivityCtrl, UICtrl){
     const loadEventListers = function(){
         // Call UISelectors
@@ -105,8 +131,15 @@ const App = function(ActivityCtrl, UICtrl){
         
         // Check for the input value in Form
         if(inputs.name !== '' && inputs.time !== ''){
-            const newItem = ActivityCtrl.addActivity(inputs.name, inputs.time);
+            const newActivity = ActivityCtrl.addActivity(inputs.name, inputs.time);
+
+        // Add activity to UI list
+        UICtrl.addUIActivity(newActivity);
         }
+
+        // Clear input form
+        UICtrl.clearInput();
+
         e.preventDefault();
     }
     
@@ -115,8 +148,14 @@ const App = function(ActivityCtrl, UICtrl){
         init: function(){
             const activities = ActivityCtrl.getActivities();
 
-            // Populate activities into UI
-            UICtrl.populateListAct(activities);
+            // Check for list activities
+            if(activities.length === 0){
+                UICtrl.hideUIList();
+            }else{
+                // Populate activities into UI
+                 UICtrl.populateListAct(activities);
+            }
+       
 
             // Call eventListeners
             loadEventListers();
